69e7faadd7f143a0d4ec956288738487ee6de8ab
Matt Wynne <matt@mattwynne.net>
2026-01-24 22:13:01 -0800

    Better list

--- spec/list.sh
+++ spec/list.sh
@@ -9,6 +9,16 @@ Describe 'yx list'
 
   It 'lists added yaks'
     When run sh -c 'yx add "Fix the bug" && yx list'
-    The output should equal "Fix the bug"
+    The output should equal "- [ ] Fix the bug"
+  End
+
+  It 'supports ls as an alias for list'
+    When run yx ls
+    The output should equal 'You have no yaks. Are you done?'
+  End
+
+  It 'supports ls as an alias for list (with yaks)'
+    When run sh -c 'yx add "Fix the bug" && yx ls'
+    The output should equal "- [ ] Fix the bug"
   End
 End
